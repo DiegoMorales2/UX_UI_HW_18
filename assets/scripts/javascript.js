4aede7d7186f18f2ab00e5ce23d720f343376992
DiegoMorales2 <155837383+DiegoMorales2@users.noreply.github.com>
2024-07-18 00:16:06 -0500

    Changed hero text for mobile

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,88 +1,88 @@
 
 
-const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
-};
+// const elts = {
+//     text1: document.getElementById("text1"),
+//     text2: document.getElementById("text2")
+// };
 
-const texts = [
-    "Hello, I am",
-    "CAMERON",
-    "I am a",
-    "UX/UI Designer",
-    "Front end Developer",
-];
-
-
-const morphTime = 1;
-const cooldownTime = 0.25;
-
-let textIndex = texts.length - 1;
-let time = new Date();
-let morph = 0;
-let cooldown = cooldownTime;
-
-elts.text1.textContent = texts[textIndex % texts.length];
-elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-
-function doMorph() {
-    morph -= cooldown;
-    cooldown = 0;
-    
-    let fraction = morph / morphTime;
-    
-    if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-    }
-    
-    setMorph(fraction);
-}
-
-function setMorph(fraction) {
-    
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-    
-    fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-    
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-}
-
-function doCooldown() {
-    morph = 0;
-    
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
-    
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
-}
+// const texts = [
+//     "Hello, I am",
+//     "CAMERON",
+//     "I am a",
+//     "UX/UI Designer",
+//     "Front end Developer",
+// ];
 
 
-function animate() {
-    requestAnimationFrame(animate);
+// const morphTime = 1;
+// const cooldownTime = 0.25;
+
+// let textIndex = texts.length - 1;
+// let time = new Date();
+// let morph = 0;
+// let cooldown = cooldownTime;
+
+// elts.text1.textContent = texts[textIndex % texts.length];
+// elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+// function doMorph() {
+//     morph -= cooldown;
+//     cooldown = 0;
     
-    let newTime = new Date();
-    let shouldIncrementIndex = cooldown > 0;
-    let dt = (newTime - time) / 1000;
-    time = newTime;
+//     let fraction = morph / morphTime;
     
-    cooldown -= dt;
+//     if (fraction > 1) {
+//         cooldown = cooldownTime;
+//         fraction = 1;
+//     }
     
-    if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-            textIndex++;
-        }
+//     setMorph(fraction);
+// }
+
+// function setMorph(fraction) {
+    
+//     elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+//     elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+    
+//     fraction = 1 - fraction;
+//     elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+//     elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+    
+//     elts.text1.textContent = texts[textIndex % texts.length];
+//     elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+// }
+
+// function doCooldown() {
+//     morph = 0;
+    
+//     elts.text2.style.filter = "";
+//     elts.text2.style.opacity = "100%";
+    
+//     elts.text1.style.filter = "";
+//     elts.text1.style.opacity = "0%";
+// }
+
+
+// function animate() {
+//     requestAnimationFrame(animate);
+    
+//     let newTime = new Date();
+//     let shouldIncrementIndex = cooldown > 0;
+//     let dt = (newTime - time) / 1000;
+//     time = newTime;
+    
+//     cooldown -= dt;
+    
+//     if (cooldown <= 0) {
+//         if (shouldIncrementIndex) {
+//             textIndex++;
+//         }
         
-        doMorph();
-    } else {
-        doCooldown();
-    }
-}
+//         doMorph();
+//     } else {
+//         doCooldown();
+//     }
+// }
 
 
-animate();
+// animate();
